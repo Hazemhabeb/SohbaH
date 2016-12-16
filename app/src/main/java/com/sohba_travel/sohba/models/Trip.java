e0671833c968509e3e40046b7d8f42296c1f62a1
hazem <hazemhabeb94@gmail.com>
2016-12-16 15:04:22 +0200

    trip modle

--- app/src/main/java/com/sohba_travel/sohba/models/Trip.java
+++ app/src/main/java/com/sohba_travel/sohba/models/Trip.java
@@ -1,126 +1,38 @@
 package com.sohba_travel.sohba.Models;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
 /**
  * Created by M on 12/8/2016.
  */
 
-public class Trip {
-    String tripId;
-    String userId;
-    String Description;
-    String tripName;
-    String tripPlace;
-    String tripType;
-    String tripPrice;
-    String tripRate;
+public class Trip implements Serializable {
+    public String tripId;
+    public String userId;
+    public String tripDescription;
+    public String tripName;
+    public String tripPlace;
+    public String tripType;
+    public String tripPrice;
+    public String tripRate;
+    public String tripImage;
 
-    public String getTripImage() {
-        return tripImage;
-    }
-
-    public void setTripImage(String tripImage) {
-        this.tripImage = tripImage;
-    }
-
-    String tripImage;
-
-    public HashMap<String, Timeline> getTimelineHashMap() {
-        return timelineHashMap;
-    }
-
-    public void setTimelineHashMap(HashMap<String, Timeline> timelineHashMap) {
-        this.timelineHashMap = timelineHashMap;
-    }
-
-    public String getTripId() {
-        return tripId;
-    }
-
-    public void setTripId(String tripId) {
-        this.tripId = tripId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getDescription() {
-        return Description;
-    }
-
-    public void setDescription(String description) {
-        Description = description;
-    }
-
-    public String getTripName() {
-        return tripName;
-    }
-
-    public void setTripName(String tripName) {
-        this.tripName = tripName;
-    }
-
-    public String getTripPlace() {
-        return tripPlace;
-    }
-
-    public void setTripPlace(String tripPlace) {
-        this.tripPlace = tripPlace;
-    }
-
-    public String getTripType() {
-        return tripType;
-    }
-
-    public void setTripType(String tripType) {
-        this.tripType = tripType;
-    }
-
-    public String getTripPrice() {
-        return tripPrice;
-    }
-
-    public void setTripPrice(String tripPrice) {
-        this.tripPrice = tripPrice;
-    }
-
-    public String getTripRate() {
-        return tripRate;
-    }
-
-    public void setTripRate(String tripRate) {
-        this.tripRate = tripRate;
-    }
-
-    public HashMap<String, SingleRating> getRatingHashMap() {
-        return RatingHashMap;
-    }
-
-    public void setRatingHashMap(HashMap<String, SingleRating> ratingHashMap) {
-        RatingHashMap = ratingHashMap;
-    }
-
-    HashMap<String, Timeline> timelineHashMap = new HashMap<String, Timeline>();
-    HashMap<String, SingleRating> RatingHashMap = new HashMap<String, SingleRating>();
+    public HashMap<String, Timeline> timelineHashMap = new HashMap<String, Timeline>();
+    public HashMap<String, SingleRating> RatingHashMap = new HashMap<String, SingleRating>();
 
     public Trip() {
     }
 
     public Trip(String tripId, String userId,
-                String Description, String tripName,
+                String tripDescription, String tripName,
                 String tripPlace, String tripType,
                 String tripPrice, String tripRate,
                 String tripImage, HashMap<String, Timeline> timelineHashMap
             , HashMap<String, SingleRating> RatingHashMap) {
         this.tripId = tripId;
         this.userId = userId;
-        this.Description = Description;
+        this.tripDescription = tripDescription;
         this.tripName = tripName;
         this.tripPlace = tripPlace;
         this.tripType = tripType;
